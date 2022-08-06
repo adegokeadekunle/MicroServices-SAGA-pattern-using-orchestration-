b942acd87a1bd6c1a2313afbfe1d6f51a0bd34b1
adegokeadekunle2 <adegokeadekunle2@gmail.com>
2022-08-06 23:50:24 +0100

    validate payment command

--- OrderService/src/main/java/com/adekunle/OrderService/command/api/saga/OrderProcessingSaga.java
+++ OrderService/src/main/java/com/adekunle/OrderService/command/api/saga/OrderProcessingSaga.java
@@ -1,22 +1,50 @@
 package com.adekunle.OrderService.command.api.saga;
 
 import com.adekunle.CommonService.commons.ValidatePaymentCommand;
+import com.adekunle.CommonService.model.User;
+import com.adekunle.CommonService.querries.GetUserPaymentQuery;
 import com.adekunle.OrderService.command.api.events.OrderCreatedEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Saga;
 
 @Saga
 @Slf4j
+@RequiredArgsConstructor
 public class OrderProcessingSaga {
+
+    private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
 
     @StartSaga  // this starts the saga
     @SagaEventHandler(associationProperty = "orderId")  // this should be the id of the order
     private void handle(OrderCreatedEvent orderCreatedEvent){
         log.info("OrderCreatedEvent in saga for order id : {}",orderCreatedEvent.getOrderId());
 
-        ValidatePaymentCommand validatePaymentCommand = ValidatePaymentCommand.builder().build();
+        GetUserPaymentQuery getUserPaymentQuery = new GetUserPaymentQuery(orderCreatedEvent.getUserId());
+
+        User user = null;
+
+        try{
+            user = queryGateway.query(getUserPaymentQuery, ResponseTypes.instanceOf(User.class)).join();
+        }catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+        ValidatePaymentCommand validatePaymentCommand = ValidatePaymentCommand.builder()
+                .PaymentId(orderCreatedEvent.getProductId())
+                .orderId(orderCreatedEvent.getOrderId())
+                .cardDetails(user.getCardDetails())
+                .build();
+        
+        commandGateway.sendAndWait(validatePaymentCommand);
+
+
     }
 
 }
--- UserService/src/main/java/com/adekunle/UserService/projection/UserProjection.java
+++ UserService/src/main/java/com/adekunle/UserService/projection/UserProjection.java
@@ -1,7 +1,31 @@
 package com.adekunle.UserService.projection;
 
+import com.adekunle.CommonService.model.CardDetails;
+import com.adekunle.CommonService.model.User;
+import com.adekunle.CommonService.querries.GetUserPaymentQuery;
+import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserProjection {
+
+    @QueryHandler
+    public User getUserDetails(GetUserPaymentQuery userPaymentQuery) {
+// ideally you get details of user from database.
+        CardDetails cardDetails = CardDetails.builder()
+                .name("Adekunle")
+                .validMonth(11)
+                .ValidYear(2024)
+                .cvv(123)
+                .cardNo("54627727388238383")
+                .build();
+
+        return  User.builder()
+                .firstName("Adekunle")
+                .LastName("Adegoke")
+                .userId(userPaymentQuery.getUserId())
+                .cardDetails(cardDetails)
+                .build();
+
+    }
 }
