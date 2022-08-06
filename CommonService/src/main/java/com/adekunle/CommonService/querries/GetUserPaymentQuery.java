3e21bd2995d9e925e6257d2f0a0b324b05452112
adegokeadekunle2 <adegokeadekunle2@gmail.com>
2022-08-06 22:14:45 +0100

    more updates on saga orchestration pattern MS

--- CommonService/src/main/java/com/adekunle/CommonService/querries/GetUserPaymentQuery.java
+++ CommonService/src/main/java/com/adekunle/CommonService/querries/GetUserPaymentQuery.java
@@ -1,4 +1,14 @@
 package com.adekunle.CommonService.querries;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetUserPaymentQuery {
+
+    private String userId;
+
 }
---
+++ UserService/src/main/java/com/adekunle/UserService/projection/UserProjection.java
@@ -0,0 +1,7 @@
+package com.adekunle.UserService.projection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserProjection {
+}
